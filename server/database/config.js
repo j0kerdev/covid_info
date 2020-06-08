7852fd59d2c321795c47d8534ca52bfdbb81515e
j0kerdev <66632114+j0kerdev@users.noreply.github.com>
2020-06-08 11:22:40 -0400

    Initial commit

--- server/database/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'archive2019',
-    dbname: process.env.DB_NAME || 'covid_info',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: process.env.DIALECT || 'mysql'
-}
